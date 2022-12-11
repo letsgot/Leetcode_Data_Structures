933342baf9a3e526ff004db43e00a75de92fddc2
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2022-12-11 13:01:46 +0530

    Time: 28 ms (50.00%), Space: 42.9 MB (100.00%) - LeetHub

---
+++ 2502-design-memory-allocator/2502-design-memory-allocator.java
@@ -0,0 +1,31 @@
+class Allocator {
+    private int[] memory; 
+    
+    public Allocator(int n) {
+        memory = new int[n]; 
+    }
+    
+    public int allocate(int size, int mID) {
+        int cnt = 0; 
+        for (int i = 0; i < memory.length; ++i) {
+            if (memory[i] == 0) {
+                if (++cnt == size) {
+                    for (int ii = i; ii >= i-size+1; --ii) 
+                        memory[ii] = mID; 
+                    return i-size+1; 
+                }
+            } else cnt = 0; 
+        }
+        return -1; 
+    }
+    
+    public int free(int mID) {
+        int ans = 0; 
+        for (int i = 0; i < memory.length; ++i) 
+            if (memory[i] == mID) {
+                ++ans; 
+                memory[i] = 0; 
+            }
+        return ans; 
+    }
+}
