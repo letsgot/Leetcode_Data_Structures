28fabfada7935bf23a7ae0fd5e3e2cee32fcfd76
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2022-12-30 19:26:30 +0530

    Time: 0 ms (100.00%), Space: 42 MB (76.16%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.java
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.java
@@ -19,47 +19,10 @@ class Solution {
     // 1 :- monitored
     // 2 :- camera installed
     
-//     int ci = 0;
-//     int m = 1;
-//     int c = 2;
-//     int n = 3;
-//     public int minCameraCover(TreeNode root) {
-//         if(root==null){
-//             return 0;
-//         }
-//         else if(root.left==null&&root.right==null){
-//             return 1;
-//         }
-//         ci =0;
-//         int v = helper(root);
-//         if(v==3){ci++;}
-//         return ci;
-//     }
-    
-//     public int helper(TreeNode root){
-//         if(root==null){
-//             return 1;
-//         }
-        
-//         int left = helper(root.left);
-//         int right = helper(root.right);
-        
-//         if(left==2||right==2){
-//             return 1;
-//         }
-//         else if(left==3||right==3){
-//             ci++;
-//             return 2;
-//         }
-//         else{
-//             return 3;
-//         }
-//     }
-    
-    int count = 0;
-    int monitored = 1;
-    int camera = 2;
-    int notMonitored = 3;
+    int ci = 0;
+    int m = 1;
+    int c = 2;
+    int n = 3;
     public int minCameraCover(TreeNode root) {
         if(root==null){
             return 0;
@@ -67,13 +30,10 @@ class Solution {
         else if(root.left==null&&root.right==null){
             return 1;
         }
-        
-        count = 0;
-        int val = helper(root);
-        if(val==3){
-            count++;
-        }
-        return count;
+        ci =0;
+        int v = helper(root);
+        if(v==3){ci++;}
+        return ci;
     }
     
     public int helper(TreeNode root){
@@ -85,13 +45,53 @@ class Solution {
         int right = helper(root.right);
         
         if(left==3||right==3){
-            count++;
+            ci++;
             return 2;
         }
         else if(left==2||right==2){
             return 1;
-        }else{
+        }
+        else{
             return 3;
         }
     }
+    
+//     int count = 0;
+//     int monitored = 1;
+//     int camera = 2;
+//     int notMonitored = 3;
+//     public int minCameraCover(TreeNode root) {
+//         if(root==null){
+//             return 0;
+//         }
+//         else if(root.left==null&&root.right==null){
+//             return 1;
+//         }
+        
+//         count = 0;
+//         int val = helper(root);
+//         if(val==3){
+//             count++;
+//         }
+//         return count;
+//     }
+    
+//     public int helper(TreeNode root){
+//         if(root==null){
+//             return 1;
+//         }
+        
+//         int left = helper(root.left);
+//         int right = helper(root.right);
+        
+//         if(left==3||right==3){
+//             count++;
+//             return 2;
+//         }
+//         else if(left==2||right==2){
+//             return 1;
+//         }else{
+//             return 3;
+//         }
+//     }
 }
