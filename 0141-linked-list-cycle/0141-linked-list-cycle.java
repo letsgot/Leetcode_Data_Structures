f0625a384e107cba861c3ac27e5585f10600dbf0
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2022-12-31 22:46:38 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (85.71%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,29 +11,29 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-//         if(head==null||head.next==null){
-//             return false;
-//         }
-//         ListNode fast = head;
-//         ListNode slow = head;
-        
-//         while(fast!=null||fast.next!=null){
-//             fast = fast.next.next;
-//             slow = slow.next;
-//             if (slow == fast) 
-//                return true;
-//         }
-        
-//         return false;
-        ListNode slow = head,fast = head;
-        if(head == null || head.next == null)return false;
-        while(fast != null && fast.next != null)
-        {
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast)
-                return true;
+        if(head==null||head.next==null){
+            return false;
         }
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast!=null&&fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) 
+               return true;
+        }
+        
+        // return false;
+        // ListNode slow = head,fast = head;
+        // if(head == null || head.next == null)return false;
+        // while(fast != null && fast.next != null)
+        // {
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        //     if(slow == fast)
+        //         return true;
+        // }
         return false;
     }
 }
