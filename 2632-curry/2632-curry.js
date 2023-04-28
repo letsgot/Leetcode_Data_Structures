7220a597002f496a614f11892f3bb6daaca5c052
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2023-04-28 14:26:41 +0530

    Time: 74 ms (34.60%), Space: 48.5 MB (39.93%) - LeetHub

---
+++ 2632-curry/2632-curry.js
@@ -0,0 +1,13 @@
+var curry = function(fn) {
+   return function curried(...args) {
+        if (fn.length === args.length) {
+            return fn(...args);
+        } else {
+            return function(...newArgs) {
+                return curried(...args, ...newArgs)
+            }
+        }
+    };
+};
+
+
