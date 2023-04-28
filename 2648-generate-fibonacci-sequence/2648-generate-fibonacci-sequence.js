5ab9d92acb2da000036f2d6b424e2a15894078e3
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2023-04-28 12:29:47 +0530

    Time: 45 ms (97.72%), Space: 42.1 MB (54.31%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,22 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let a = 0;
+    let b = 1;
+    yield a;
+    yield b;
+    
+    while(true){
+        let c = a + b;
+        yield c;
+        a = b;
+        b = c;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
