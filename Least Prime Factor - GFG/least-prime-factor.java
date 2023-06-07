d122ad97ddafca5bbb82bf9571b0ba2c3b439cd7
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2023-06-07 11:11:17 +0530

    Added solution - LeetHub

---
+++ Least Prime Factor - GFG/least-prime-factor.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int[] ans = ob.leastPrimeFactor(n);
+            for(int i = 1; i <=n; i++)
+            {
+                out.print(ans[i] + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    public int prime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0)
+                return i;
+        }
+        return n;
+    }
+    public int[] leastPrimeFactor(int n)
+    {
+        // code here
+        int[] res = new int[n+1];
+        res[1] = 1;
+        for(int i = 2; i <= n;i++){
+            if(res[i]!=0){
+                continue;
+            }
+           
+            res[i] = prime(i);
+            for(int j=i;j<=n;j+=i){
+                if(res[j]!=0){
+                    continue;
+                }
+                res[j] = i;
+            }
+            
+        }
+        return res;
+    }
+}
