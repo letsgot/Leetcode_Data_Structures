cfdb157ba2f7c9932613a57a910d18038e20abad
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2023-06-09 09:30:27 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (8.23%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,17 +1,44 @@
 class Solution {
-    public char nextGreatestLetter(char[] a, char x) {
-        int n = a.length;
-       
-        if (x >= a[n - 1])   x = a[0];
-        else    x++;
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
         
-        int lo = 0, hi = n - 1;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (a[mid] == x)    return a[mid];
-            if (a[mid] < x)     lo = mid + 1;
-            else    hi = mid;
+        if(target>=letters[n-1]){
+            target = letters[0];
         }
-        return a[hi];
+        else {
+            target++;
+        }
+        
+        int lo = 0;
+        int hi = n-1;
+        
+        while(lo<hi){
+            int mid = lo + (hi-lo)/2;
+            if(target==letters[mid]){
+                return target;
+            }
+            else if(target<=letters[mid]){
+                hi = mid;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        
+        return letters[hi];
     }
 }
+
+//    int n = a.length;
+       
+//         if (x >= a[n - 1])   x = a[0];
+//         else    x++;
+        
+//         int lo = 0, hi = n - 1;
+//         while (lo < hi) {
+//             int mid = lo + (hi - lo) / 2;
+//             if (a[mid] == x)    return a[mid];
+//             if (a[mid] < x)     lo = mid + 1;
+//             else    hi = mid;
+//         }
+//         return a[hi];
