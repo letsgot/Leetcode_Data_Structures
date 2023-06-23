ff12dbb8552e05f4a1c008bfcaf0da0b1fb855da
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2023-06-23 13:18:38 +0530

    Time: 488 ms (79.28%), Space: 58.9 MB (68.06%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
@@ -1,15 +1,36 @@
 class Solution {
-    public int longestArithSeqLength(int[] A) {
-            int res = 2, n = A.length;
-            HashMap<Integer, Integer>[] dp = new HashMap[n];
-            for (int j = 0; j < A.length; j++) {
-                dp[j] = new HashMap<>();
-                for (int i = 0; i < j; i++) {
-                    int d = A[j] - A[i];
-                    dp[j].put(d, dp[i].getOrDefault(d, 1) + 1);
-                    res = Math.max(res, dp[j].get(d));
+    public int longestArithSeqLength(int[] nums) {
+        if(nums.length<=2){
+            return 2;
+        }
+        
+        // Arrays.sort(nums);
+        
+        HashMap<Integer,Integer> [] arr = new HashMap[nums.length];
+        
+        for(int i=0;i<nums.length;i++){
+            arr[i] = new HashMap<>();
+        }
+        
+        int ans = 2;
+        
+        for(int i=nums.length-2;i>=0;i--){
+            for(int j=nums.length-1;j>i;j--){
+                int val = nums[j] - nums[i];
+                
+                HashMap<Integer,Integer> m = arr[j];
+                HashMap<Integer,Integer> mi = arr[i];
+                if(m.containsKey(val)){
+                    int max = m.get(val)+1;
+                    mi.put(val,max);
+                    ans = Math.max(ans,max);
+                }
+                else{
+                    mi.put(val,2);
                 }
             }
-            return res;
+        }
+        
+        return ans;
     }
 }
