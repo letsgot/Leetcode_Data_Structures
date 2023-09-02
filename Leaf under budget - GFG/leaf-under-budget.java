e0c193c4dc500ffede7381bcc3493d62ad03bd02
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2023-09-02 08:21:58 +0530

    Added solution - LeetHub

---
+++ Leaf under budget - GFG/leaf-under-budget.java
@@ -0,0 +1,174 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+class GFG {
+    
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null)
+            return;
+        
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+		    String s = br.readLine();
+	    	Node root = buildTree(s);
+	    	int k = Integer.parseInt(br.readLine());
+		    Solution obj = new Solution();
+		    int res = obj.getCount(root,k);
+		    System.out.println(res);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution{
+    public int getCount(Node node, int bud)
+    {
+        //code here  
+        Queue<Node> que = new ArrayDeque<>();
+        que.add(node);
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int lvl = 0;
+        while(que.size()!=0){
+            lvl++;
+            int size = que.size();
+            for(int i=0;i<size;i++){
+                Node rem = que.remove();
+                
+                // System.out.print(rem.data + " ");
+                
+                if(rem.left==null&&rem.right==null){
+                    list.add(lvl);
+                }
+               
+                if(rem.left!=null&&rem.right!=null){
+                    que.add(rem.left);
+                    que.add(rem.right);
+                }
+                else  if(rem.left!=null){
+                    que.add(rem.left);
+                }
+                else if(rem.right!=null){
+                    que.add(rem.right);
+                }
+            }
+        }
+        
+        Collections.sort(list);
+        // while(idx<list.size()){
+        //     int val = list.get(idx);
+        //     // System.out.println(val);
+        //     idx++;
+        // }
+        
+        int count = 0;
+        int sum = 0;
+        
+        int idx = 0;
+     
+        
+        while(idx<list.size()){
+            int val = list.get(idx);
+            sum = sum + val;
+            if(sum>bud){
+                break;
+            }
+            count++;
+            idx++;
+        }
+        
+        return count;
+    }
+}
