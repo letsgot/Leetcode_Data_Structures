0a029620e1f7147d656eb334f78bd629968fb937
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2023-10-13 23:58:07 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (55.40%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -9,99 +9,42 @@
  * }
  */
 class Solution {
-    public ListNode[] splitListToParts(ListNode head, int k) {
-       
-        ListNode ans[] = new ListNode[k];
-        if(head==null){
-            return ans;
-        }
-        
-        Arrays.fill(ans,null);
-        
-        
-        int size = siz(head);
-        int eachLength = size / k;
-        int xtraLength = size % k;
-        
-        int index = 0;
-        ListNode curr = head;
+    public int size(ListNode curr){
+        int sz = 0;
         while(curr!=null){
-            int diff = xtraLength<=0?0:1;
-            xtraLength--;
-            
-            ans[index++] = curr;
-            for(int i=0;i<eachLength+diff-1;i++){
-                curr = curr.next;
+            sz++;
+            curr = curr.next;
+        }
+        return sz;
+    }
+    
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int size = size(head);
+        
+        int q = size / k;
+        int rem = size % k;
+        
+        ListNode [] ans = new ListNode[k];
+        int i = 0;
+        
+        while(head!=null){
+            int c = q + (rem>0?1:0);
+            rem--;
+            ans[i] = head;
+            i++;
+            while(head!=null&&c>0){
+                if(c==1){
+                    c--;
+                    ListNode curr = head.next;
+                    head.next = null;
+                    head = curr;
+                    continue;
+                }
+                head = head.next;
+                c--;
             }
-            
-            ListNode temp = curr.next;
-            curr.next = null;
-            curr = temp;
         }
         
         return ans;
-        
-    }
-    
-    
-    public int siz(ListNode node){
-        int co = 0;
-        while(node!=null){
-            co++;
-            node = node.next;
-        }
-        return co;
     }
 }
-
-// class Solution {
-//     public ListNode[] splitListToParts(ListNode root, int k) {
-//         // prepare the results for each of the buckets upto k size
-// 		ListNode[] result = new ListNode[k];
-        
-// 		// if root is null then return the empty array
-//         if (root == null)
-//             return result;
-        
-//         // find the total length of the list
-//         int length = 0;
-//         ListNode curr = root;
-//         while (curr != null){
-//             length++;
-//             curr = curr.next;
-//         }
-        
-//         //find the length of each bucket in k buckets 
-// 		int eachLength = length / k;
-//         //not every time we get equal size for all the buckets so sometimes some of the bucket will have extra elements in it
-// 		//so we have to find the length of extra buckets by mod operation
-//        	int extraLength = length % k;
-        
-// 		int index = 0;
-//         curr = root;
-//         //loop for all the elements in the list
-//         while (curr != null){
-//             //for every bucket we start with the current element
-// 			ListNode head = curr;
-            
-//             //each part difference is either 0 or 1, we use the reminder to identify the difference
-//             int difference = extraLength <= 0 ? 0 : 1;
-            
-//             //move the pointer to the size of eachlength
-//             for(int i=0;i< eachLength + difference - 1;i++)
-//                 curr = curr.next;
-            
-//             //set the next to null
-//             ListNode temp = curr.next;
-//             curr.next = null;
-//             curr = temp;
-            
-//             //add to the result array
-//             result[index++] = head;
-            
-//             //decrement the extra length
-//             extraLength--;
-//         }       
-//         return result;
-//     }
-// }
