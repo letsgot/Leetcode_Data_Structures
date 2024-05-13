58f9a24298e4a578e9cf470bd7ce1249cf06de10
Vivek Chauhan <87446633+letsgot@users.noreply.github.com>
2024-05-13 17:25:29 +0530

    Time: 54 ms (43.00%), Space: 49.2 MB (6.23%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe : (innerVal) => {
+            if(innerVal===val) {
+                return  true
+            }
+            else{
+                throw "Not Equal"
+            }
+        },
+        notToBe : (innerVal) => {
+            if(innerVal!==val) {
+                return  true
+            }
+            else{
+               throw  "Equal"
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
